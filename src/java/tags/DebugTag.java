f284e8f716769107b9cef4dfde12501259a0491f
728646 <728646@ELF6IT216371.ACDM.DS.SAIT.CA>
2017-10-31 11:06:44 -0600

    Modified tlds and wrote debugTag

--- src/java/tags/DebugTag.java
+++ src/java/tags/DebugTag.java
@@ -1,5 +1,19 @@
 package tags;
 
-public class DebugTag {
-    
+import javax.servlet.ServletRequest;
+import javax.servlet.jsp.JspException;
+import javax.servlet.jsp.tagext.TagSupport;
+
+public class DebugTag extends TagSupport {
+
+    @Override
+    public int doStartTag() throws JspException {
+        ServletRequest request = pageContext.getRequest();
+        if (request.getParameter("debug") != null && request.getServerName().startsWith("test")){
+            return EVAL_BODY_INCLUDE; 
+        }
+        else{
+            return SKIP_BODY; 
+        }
+    }  
 }
